fbeda5eb01d51e262de68d08db606f97a7ebb8e1
cassplouis <cassandraplouis@gmail.com>
2017-06-21 20:58:20 -0400

    june.21.2017

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,10 +7,19 @@ angular.module('starter.controllers', [])
   $scope.remove = function(task) {
     Tasks.remove(task);
   };
-})
+
+
+  $ionicModal.fromTemplateUrl("templates/modal.html", )
+   	 scope: $scope, 
+   	 animation: "slide-in-up"
+  }).then(function(modal){
+  	$scope.modal = modal; 
+  }); 
 
 .controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
   $scope.task = Tasks.get($stateParams.taskId);
 })
+
+
 
 .controller('AccountCtrl', function($scope) {});
